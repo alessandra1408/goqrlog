7516cfbe10210ec981887144e0773809e3c4cb30
alessandra1408 <alessandrafernandes.contato@gmail.com>
2025-07-23 00:08:34 -0300

    chore: adds block for duplication call

--- app/qrcode/qrcode.go
+++ app/qrcode/qrcode.go
@@ -2,6 +2,8 @@ package qrcode
 
 import (
 	"context"
+	"fmt"
+	"sync"
 	"time"
 
 	"github.com/alessandra1408/goqrlog/internal/config"
@@ -9,6 +11,8 @@ import (
 	"github.com/alessandra1408/goqrlog/pkg/log"
 	"github.com/jackc/pgx/v5"
 )
+
+var lastRequests sync.Map
 
 type App interface {
 	QRCodeHandler(ctx context.Context, req *Request, log log.Log) (pgx.Rows, error)
@@ -29,6 +33,17 @@ func NewApp(cfg config.Config, db *db.Database) App {
 func (a *app) QRCodeHandler(ctx context.Context, req *Request, log log.Log) (pgx.Rows, error) {
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
+
+	key := fmt.Sprintf("%d", req.MatriculaAluno)
+	now := time.Now().Unix()
+
+	if val, ok := lastRequests.Load(key); ok {
+		if last, ok := val.(int64); ok && now-last < 3 {
+			log.Warnf("Ignored duplicate request for matricula %d", req.MatriculaAluno)
+			return nil, nil // ou retornar erro de Too Many Requests
+		}
+	}
+	lastRequests.Store(key, now)
 
 	log.Info("QRCodeHandler called")
 
